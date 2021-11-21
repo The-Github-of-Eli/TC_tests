17604ee812d58d35d1d024879ac0c10e21536221
The-Github-of-Eli <46939218+The-Github-of-Eli@users.noreply.github.com>
2021-11-21 16:20:01 +0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,16 @@
+import pytest
+from teamcity import is_running_under_teamcity
+
+
+def test_1():
+    assert True
+
+def test_2():
+    assert True
+
+def test_3():
+    assert False
+
+def test_4():
+    assert True
+
