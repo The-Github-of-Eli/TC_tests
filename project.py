58877576d7fcd8772bc39f95ba5f11922a7fecd3
The-Github-of-Eli <46939218+The-Github-of-Eli@users.noreply.github.com>
2021-11-21 15:45:30 +0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_1():
+    assert True
+
+def test_2():
+    assert True
+
+def test_3():
+    assert False
+
+def test_4():
+    assert True
+import sys
+
+print(sys.executable)
